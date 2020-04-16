ee607fc7629417a871b98fd3933183c11a97f04f
Jonas <jokla17@student.sdu.dk>
2020-04-16 13:59:56 +0200

    The system now recognizes which user is logged in and shows it on the homepage.

--- src/main/java/presentation/HomeController.java
+++ src/main/java/presentation/HomeController.java
@@ -1,14 +1,22 @@
 package presentation;
 
+import domain.AuthentificationManager;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController extends MainController implements Initializable {
+
+    @FXML
+    private Label currentUserLbl;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         vbSideBarLogo.setImage(new Image(new File("logo-ajate.png").toURI().toString()));
+        currentUserLbl.setText(AuthentificationManager.getCurrentUser().getUsername());
     }
 }
